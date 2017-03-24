acdeb1a14bbfe2d8aa04f6233ff28295e94ae4c3
Keith Franklin <keith30895@gmail.com>
2017-03-24 11:50:55 +0530

    root route

--- app.js
+++ app.js
@@ -5,14 +5,15 @@ var mongoose = require('mongoose');
 
 
 var User = require("./models/user");
-
+mongoose.connect("mongodb://admin:cloudnine@ds117829.mlab.com:17829/festmamu"); // public hosted mongo db
+// mongoose.connect("mongodb://localhost/pheno_bot");
 var converRoute = require("./routes/conv")
  
  app.use("/api/messages",converRoute);
 app.get('/',function(req,res){
 	res.send("hi");
 });
-mongoose.connect("mongodb://localhost/pheno_bot");
+
   app.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('server started');
    console.log(process.env.PORT) ;
