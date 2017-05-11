0fe5593dd9d8a1a4f9ee777f0455701357980893
Keith Franklin <keith30895@gmail.com>
2017-05-11 18:53:35 +0530

    mongo correction

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ var express = require("express");
 var app     = express();
 var mongoose = require('mongoose');
 
-
 // var User = require("./models/user");
-mongoose.connect("mongodb://admin:phenobot@ds141410.mlab.com:41410/keithtrials"); // public hosted mongo db
+mongoose.connect("mongodb://keith:keithpassword@ds141410.mlab.com:41410/keithtrials"); // public hosted mongo db
 
-mongoose.connect("mongodb://localhost/pheno_bot");
+// mongoose.connect("mongodb://localhost/pheno_bot");
 var converRoute = require("./routes/conv")
  
  app.use("/api/messages",converRoute);
@@ -15,7 +14,7 @@ app.get('/',function(req,res){
 	res.send("hi");
 });
 
-  app.listen(process.env.port || process.env.PORT || 3978, function () {
+  app.listen(process.env.port || process.env.PORT, function () {
    console.log('server started');
    console.log(process.env.PORT) ;
 });
