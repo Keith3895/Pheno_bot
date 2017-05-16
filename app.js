953d54a2cf12cea8d322c87a8deee3051eb7d53b
Keith Franklin <keith30895@gmail.com>
2017-05-16 22:07:43 +0530

    heroku push

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require("express");
 var app     = express();
 var mongoose = require('mongoose');
 require('dotenv').config();
+
 // var User = require("./models/user");
 mongoose.connect("mongodb://"+process.env.DB_Host+":"+process.env.DB_Password+"@ds141410.mlab.com:41410/keithtrials"); // public hosted mongo db
 
@@ -14,8 +15,9 @@ app.get('/',function(req,res){
 	res.send("hi");
 });
 
-  app.listen(process.env.port || process.env.PORT, function () {
+  app.listen(process.env.port || process.env.PORT , function () {
    console.log('server started');
    console.log(process.env.PORT) ;
+   console.log(process.env.port) ;
 });
  
